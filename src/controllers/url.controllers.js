e94797aa6846b90f02086e783fbdeb3ff9c3e7dc
gabriel-victor933 <gabrielvictor933@usp.br>
2023-05-19 16:20:05 -0300

    feat: geturl route

--- src/controllers/url.controllers.js
+++ src/controllers/url.controllers.js
@@ -9,3 +9,17 @@ export async function postUrl(req,res){
         VALUES ($1,$2,$3) RETURNING id,shorturl`,[res.locals.userId,req.body.url,shortUrl])
     res.status(201).send(data.rows[0])
 }
+
+export async function getUrl(req,res){
+
+    const id = parseInt(req.params.id)
+
+    if(!id) return res.status(400).send("invalid id")
+
+    const link = await db.query(`SELECT id,shorturl,url FROM shortlinks WHERE shortlinks.id = $1`,[req.params.id])
+
+    if(link.rowCount === 0) return res.status(404).send("URl doenst exist")
+
+    
+    res.send(link.rows[0])
+}
--- src/routers/url.router.js
+++ src/routers/url.router.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { validateUrl } from "../middlewares/url.middleware.js";
-import { postUrl } from "../controllers/url.controllers.js";
+import { postUrl, getUrl } from "../controllers/url.controllers.js";
 import { authToken } from "../middlewares/auth.middleware.js";
 
 const route = Router()
 
 route.post("/urls/shorten",authToken,validateUrl,postUrl)
+
+route.get("/urls/:id",getUrl)
+
 
 export default route
